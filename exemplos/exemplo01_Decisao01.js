b5dbdc92f95f4ff4aba06682e4fbbb17b08ded3d
ferreiraalan13 <ferreiraalan13@gmail.com>
2023-10-01 19:38:08 -0300

    commit exemplos

---
+++ exemplos/exemplo01_Decisao01.js
@@ -0,0 +1,10 @@
+idade = 20
+formacao = "Desenvolvimento de sistemas"
+
+if (idade >= 18 && formacao == "Desenvolvimento de sistemas"){
+    console.log("APROVADO")
+}
+else{
+    console.log("REPROVADO")
+}
+
---
+++ exemplos/exemplo02_Decisao02.js
@@ -0,0 +1,10 @@
+idade = 20
+formacao = "Desenvolvimento de sistemas"
+
+if (idade >= 18 && (formacao == "Desenvolvimento de sistemas" || formacao == "Informatica para internet") ){
+    console.log("APROVADO")
+}
+else{
+    console.log("REPROVADO")
+}
+
